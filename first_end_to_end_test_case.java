801b320aeb97c9dd63b818aa78fa6654f66c29b5
saurabh2k26 <saurabh2k26@gmail.com>
2020-10-12 09:49:15 +0530

    Create first_end_to_end_test_case.java

---
+++ first_end_to_end_test_case.java
@@ -0,0 +1,41 @@
+package MyFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.junit.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstEndToEndTestCase {
+  public static void main(String[] args) {
+    System.setProperty("webdriver.gecko.driver", "//Users//sshrivastava//Documents//selenium//geckodriver");
+    WebDriver driver = new FirefoxDriver();
+    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+    driver.get("http://twitter.com");
+    //driver.findElement(By.cssSelector("a[data-testid='loginButton']")).click();
+    driver.findElement(By.name("session[username_or_email]")).sendKeys("letstweet_a");
+    driver.findElement(By.name("session[password]")).sendKeys("donttellk3bever");
+    driver.findElement(By.cssSelector("div[data-testid='LoginForm_Login_Button']")).click();
+    driver.findElement(By.cssSelector("a[aria-label='Profile']")).click();
+    driver.findElement(By.cssSelector("a[href='/settings/profile']")).click();
+    driver.findElement(By.cssSelector("input[name='displayName']")).clear();
+    driver.findElement(By.cssSelector("input[name='displayName']")).sendKeys("aA1!@#$%^&*()");
+    driver.findElement(By.cssSelector("div[data-testid='Profile_Save_Button']")).click();
+    try
+    {
+      Thread.sleep(2000);
+    }
+       catch(Exception e)
+    {
+      System.out.println(e);
+    }
+    driver.findElement(By.cssSelector("a[href='/settings/profile']")).click();
+    String name = driver.findElement(By.cssSelector("input[name='displayName']")).getAttribute("value");
+    Assert.assertEquals(name, "aA1!@#$%^&*()");
+    driver.findElement(By.cssSelector("input[name='displayName']")).clear();
+    driver.findElement(By.cssSelector("input[name='displayName']")).sendKeys("VK");
+    driver.findElement(By.cssSelector("div[data-testid='Profile_Save_Button']")).click();
+    driver.quit();
+  }
+}
