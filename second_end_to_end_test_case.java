466d57a5d6e49dca30dfca18fa8461f0f7b74329
saurabh2k26 <saurabh2k26@gmail.com>
2020-10-12 09:50:10 +0530

    Create second_end_to_end_test_case.java

---
+++ second_end_to_end_test_case.java
@@ -0,0 +1,37 @@
+package MyFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.junit.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+public class SecondEndToEndTestCase {
+  public static void main(String[] args) {
+    System.setProperty("webdriver.gecko.driver", "//Users//sshrivastava//Documents//selenium//geckodriver");
+    WebDriver driver = new FirefoxDriver();
+    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+    driver.get("http://twitter.com");
+    //driver.findElement(By.cssSelector("a[data-testid='loginButton']")).click();
+    driver.findElement(By.name("session[username_or_email]")).sendKeys("letstweet_a");
+    driver.findElement(By.name("session[password]")).sendKeys("donttellk3bever");
+    driver.findElement(By.cssSelector("div[data-testid='LoginForm_Login_Button']")).click();
+    driver.findElement(By.cssSelector("a[aria-label='Profile']")).click();
+    driver.findElement(By.cssSelector("a[href='/settings/profile']")).click();
+    driver.findElement(By.cssSelector("input[name='displayName']")).clear();
+    //driver.findElement(By.cssSelector("input[name='displayName']")).sendKeys("VK");
+    driver.findElement(By.cssSelector("div[data-testid='Profile_Save_Button']")).click();
+    try
+    {
+      Thread.sleep(2000);
+    }
+    catch(Exception e)
+    {
+      System.out.println(e);
+    }
+    Boolean errorMsgDisplayed = driver.findElement(By.cssSelector("div[data-testid='Profile_Save_Button']")).isDisplayed();
+    Assert.assertEquals(errorMsgDisplayed, true);
+    driver.quit();
+  }
+}
